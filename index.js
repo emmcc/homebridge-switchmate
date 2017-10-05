08e814a8bba8ffbd3da5a760a5bf27461cfef9bd
valkjsaaa <jackieyang51@gmail.com>
2017-10-04 17:40:39 -0700

    Make to work with node-switchmate3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-var SwitchmateManager = require('./SwitchmateManager');
+var Switchmate3Manager = require('./Switchmate3Manager');
 var Accessory, Service, Characteristic, UUIDGen;
 
 module.exports = function (homebridge) {
@@ -9,72 +9,71 @@ module.exports = function (homebridge) {
     Characteristic = homebridge.hap.Characteristic;
     UUIDGen = homebridge.hap.uuid;
 
-    homebridge.registerPlatform("homebridge-switchmate", "Switchmate", SwitchmatePlatform);
+    homebridge.registerPlatform("homebridge-switchmate3", "Switchmate3", Switchmate3Platform);
 };
 
-function SwitchmatePlatform(log, config, api) {
+function Switchmate3Platform(log, config, api) {
     this.log = log;
     this.config = config;
-    this.switchmates = this.config.switchmates || [];
+    this.switchmate3s = this.config.switchmate3s || [];
     this.accessories = [];
-    this.SmManager = new SwitchmateManager();
+    this.SmManager = new Switchmate3Manager();
 
     if (api) {
         this.api = api;
         this.api.on('didFinishLaunching', this.didFinishLaunching.bind(this));
     }
 }
-;
 
-SwitchmatePlatform.prototype.configureAccessory = function (accessory) {
+Switchmate3Platform.prototype.configureAccessory = function (accessory) {
     var accessoryId = accessory.context.id;
 
     this.setService(accessory);
     this.accessories[accessoryId] = accessory;
 };
 
-SwitchmatePlatform.prototype.didFinishLaunching = function () {
+Switchmate3Platform.prototype.didFinishLaunching = function () {
     var platform = this;
-    if (!this.switchmates.length) {
-        platform.log.error("No Switchmates configured. Please check your 'config.json' file!");
+    if (!this.switchmate3s.length) {
+        platform.log.error("No Switchmate3s configured. Please check your 'config.json' file!");
     } else
     {
-        for (var i in platform.switchmates) {
-            var data = platform.switchmates[i];
-            data.name = "Switchmate " + data.id.substring(data.id.length - 6);
+        for (var i in platform.switchmate3s) {
+            var data = platform.switchmate3s[i];
+            data.name = "Switchmate3 " + data.id.substring(data.id.length - 6);
             platform.log("Adding " + data.name + ": (" + data.displayName + ")");
             platform.addAccessory(data);
         }
 
         for (var id in platform.accessories) {
-            var switchmate = platform.accessories[id];
-            if (!switchmate.reachable) {
-                platform.removeAccessory(switchmate);
+            var switchmate3 = platform.accessories[id];
+            if (!switchmate3.reachable) {
+                platform.removeAccessory(switchmate3);
             }
         }
         platform.SmManager.event.on('smSetup', function (smid) {
-            var mySwitchmate = platform.accessories[smid];
-            mySwitchmate.getService(Service.Switch)
+            var mySwitchmate3 = platform.accessories[smid];
+            mySwitchmate3.getService(Service.Switch)
                     .getCharacteristic(Characteristic.On).getValue();
         });
         platform.SmManager.event.on('smToggleStateChange', function (smid, state)
         {
-            var mySwitchmate = platform.accessories[smid];
-            mySwitchmate.getService(Service.Switch)
+            var mySwitchmate3 = platform.accessories[smid];
+            mySwitchmate3.getService(Service.Switch)
                     .getCharacteristic(Characteristic.On).getValue();
         });
-        platform.SmManager.Initialize(platform.switchmates);
+        platform.SmManager.Initialize(platform.switchmate3s);
     }
 };
 
-SwitchmatePlatform.prototype.addAccessory = function (data) {
+Switchmate3Platform.prototype.addAccessory = function (data) {
     if (!this.accessories[data.id]) {
         var uuid = UUIDGen.generate(data.id);
 
         var newAccessory = new Accessory(data.id, uuid, 8);
 
         newAccessory.reachable = true;
-        newAccessory.context.name = "Switchmate " + data.id.substring(data.id.length - 6);
+        newAccessory.context.name = "Switchmate3 " + data.id.substring(data.id.length - 6);
         newAccessory.context.displayName = data.displayName;
         newAccessory.context.id = data.id;
 
@@ -82,7 +81,7 @@ SwitchmatePlatform.prototype.addAccessory = function (data) {
 
         this.setService(newAccessory);
 
-        this.api.registerPlatformAccessories("homebridge-switchmate", "Switchmate", [newAccessory]);
+        this.api.registerPlatformAccessories("homebridge-switchmate3", "Switchmate3", [newAccessory]);
     } else {
         var newAccessory = this.accessories[data.id];
 
@@ -94,17 +93,17 @@ SwitchmatePlatform.prototype.addAccessory = function (data) {
     this.accessories[data.id] = newAccessory;
 };
 
-SwitchmatePlatform.prototype.removeAccessory = function (accessory) {
+Switchmate3Platform.prototype.removeAccessory = function (accessory) {
     if (accessory) {
         var name = accessory.context.name;
         var id = accessory.context.id;
-        this.log.warn("Removing Switchmate: " + name + ". No longer configured.");
-        this.api.unregisterPlatformAccessories("homebridge-switchmate", "Switchmate", [accessory]);
+        this.log.warn("Removing Switchmate3: " + name + ". No longer configured.");
+        this.api.unregisterPlatformAccessories("homebridge-switchmate3", "Switchmate3", [accessory]);
         delete this.accessories[id];
     }
 };
 
-SwitchmatePlatform.prototype.setService = function (accessory) {
+Switchmate3Platform.prototype.setService = function (accessory) {
     accessory.getService(Service.Switch)
             .getCharacteristic(Characteristic.On)
             .on('set', this.setToggleState.bind(this, accessory.context))
@@ -113,10 +112,10 @@ SwitchmatePlatform.prototype.setService = function (accessory) {
     accessory.on('identify', this.identify.bind(this, accessory.context));
 };
 
-SwitchmatePlatform.prototype.getInitState = function (accessory, data) {
+Switchmate3Platform.prototype.getInitState = function (accessory, data) {
     var info = accessory.getService(Service.AccessoryInformation);
 
-    accessory.context.manufacturer = "Switchmate";
+    accessory.context.manufacturer = "Switchmate3";
     info.setCharacteristic(Characteristic.Manufacturer, accessory.context.manufacturer);
 
     accessory.context.model = data.model || "";
@@ -129,24 +128,23 @@ SwitchmatePlatform.prototype.getInitState = function (accessory, data) {
             .getValue();
 };
 
-SwitchmatePlatform.prototype.setToggleState = function (mySwitchmate, powerState, callback) {
-
+Switchmate3Platform.prototype.setToggleState = function (mySwitchmate3, powerState, callback) {
     var platform = this;
-    platform.log("Setting %s (%s) to: %s", mySwitchmate.displayName, mySwitchmate.name, (powerState ? "ON" : "OFF"));
-    powerState ? platform.SmManager.On(mySwitchmate.id) : platform.SmManager.Off(mySwitchmate.id);
+    platform.log("Setting %s (%s) to: %s", mySwitchmate3.displayName, mySwitchmate3.name, (powerState ? "ON" : "OFF"));
+    powerState ? platform.SmManager.On(mySwitchmate3.id) : platform.SmManager.Off(mySwitchmate3.id);
     callback();
 };
 
-SwitchmatePlatform.prototype.getToggleState = function (mySwitchmate, callback) {
+Switchmate3Platform.prototype.getToggleState = function (mySwitchmate3, callback) {
     var platform = this;
-    var status = platform.SmManager.GetSwitchmateState(mySwitchmate.id);
-    platform.log("Status of %s (%s) is: %s", mySwitchmate.displayName, mySwitchmate.name, (status ? "ON" : "OFF"));
+    var status = platform.SmManager.GetSwitchmate3State(mySwitchmate3.id);
+    platform.log("Status of %s (%s) is: %s", mySwitchmate3.displayName, mySwitchmate3.name, (status ? "ON" : "OFF"));
     callback(null, status);
 
 };
 
-SwitchmatePlatform.prototype.identify = function (mySwitchmate, paired, callback) {
+Switchmate3Platform.prototype.identify = function (mySwitchmate3, paired, callback) {
     var platform = this;
-    platform.log("Identify requested for " + mySwitchmate.name);
+    platform.log("Identify requested for " + mySwitchmate3.name);
     callback();
 };
